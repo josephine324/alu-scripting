8dce8cc489eed64f4851c5ab301207960f2d5538
josephine324 <j.mutesi@alustudent.com>
2023-11-12 08:13:00 -0800

    Done

--- api_advanced/3-count.py
+++ api_advanced/3-count.py
@@ -1,50 +1,30 @@
 #!/usr/bin/python3
-""""3-count.py"""
+"""
+1-main
+"""
 import requests
 
 
-def count_words(subreddit, word_list, after="", words_count={}):
-    """"count words"""
-    url = "https://www.reddit.com/r/{}/hot.json?limit=100" \
-        .format(subreddit)
-    header = {'User-Agent': 'Mozilla/5.0'}
-    param = {'after': after}
-    response = requests.get(url, headers=header, params=param)
-
+def count_words(subreddit, word_list):
+    """
+    1-main
+    """
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    headers = {'User-Agent': 'cynt user agent 1.1'}
+    response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code != 200:
+
+        return None
+    posts = response.json().get('data').get('children')
+    word_count = {}
+    for post in posts:
+        title = post['data']['title']
+        for word in word_list:
+            if word.lower() in title.lower():
+                word_count[word.lower()] = word_count.get(word.lower(), 0) + 1
+
+    if not word_count:
         return
-
-    json_res = response.json()
-    after = json_res.get('data').get('after')
-    has_next = after is not None
-    hot_titles = []
-    words = [word.lower() for word in word_list]
-
-    if len(words_count) == 0:
-        words_count = {word: 0 for word in words}
-
-    hot_articles = json_res.get('data').get('children')
-    [hot_titles.append(article.get('data').get('title'))
-     for article in hot_articles]
-
-    # loop through all titles
-    for i in range(len(hot_titles)):
-        for title_word in hot_titles[i].lower().split():
-            for word in words:
-                if word.lower() == title_word:
-                    words_count[word] = words_count.get(word) + 1
-
-    if has_next:
-        return count_words(subreddit, word_list, after, words_count)
-    else:
-
-        words_count = dict(filter(lambda item: item[1] != 0,
-                                  words_count.items()))
-
-        words_count = sorted(words_count.items(),
-                             key=lambda item: item[1],
-                             reverse=True)
-
-        for i in range(len(words_count)):
-            print("{}: {}".format(words_count[i][0],
-                                  words_count[i][1]))
+    for key, value in sorted(word_count.items(), key=lambda x: (-x[1], x[0])):
+        print("{}: {}".format(key.lower(), value))
+    return count_words(subreddit, word_list)
